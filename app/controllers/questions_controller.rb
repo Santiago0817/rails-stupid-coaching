763184dd1e592e6cf125ca89290d6f629f6459f8
Santiago Meneses <scordoba0817@gmail.com>
2022-09-08 20:00:34 -0500

    rails stupid coaching challenge semi completed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,9 +3,11 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    @question = params["question"]
-  #   if @question.include? ()
-  #   @answer =
-  # end
+    @question = params['question']
+      if @question.include?("Hello")
+        @answer = "-I don't care, get dressed and go to work"
+      elsif @question.include?("shopping")
+        @answer = 'Silly question, get dressed and go to work'
+      end
   end
 end
